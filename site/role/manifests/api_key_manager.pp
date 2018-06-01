6a8f9c046070765affbc1a4e2c225e2fce3393d1
于鑫蕾 <xinlei.yu@capgemini.com>
2018-06-01 17:04:35 +0800

    fix naming error

--- site/role/manifests/api_key_manager.pp
+++ site/role/manifests/api_key_manager.pp
@@ -1,4 +1,4 @@
-class role::api_key_mamager {
+class role::api_key_manager {
   #This role would be made of all the profiles that need to be included to make a enterprise integrator work
   #All roles should include the base profile
   include profile::base
